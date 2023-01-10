b90555e89893ce4b5cfc2907827013af65f8b20e
Jimmy Nguyen <55465528+PrettyLegit0101@users.noreply.github.com>
2023-01-09 20:04:43 -0500

    handling incoming notifications

--- src/App.js
+++ src/App.js
@@ -1,12 +1,65 @@
 import { registerRootComponent } from "expo";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Button, View } from "react-native";
+import * as Notifications from "expo-notifications";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => {
+    return {
+      shouldPlaySound: false,
+      shouldSetBadge: false,
+      shouldShowAlert: true,
+    };
+  },
+});
+
+const allowsNotificationsAsync = async () => {
+  const settings = await Notifications.getPermissionsAsync();
+  return (
+    settings.granted ||
+    settings.ios?.status === Notifications.IosAuthorizationStatus.PROVISIONAL
+  );
+};
+
+const requestPermissionsAsync = async () => {
+  return await Notifications.requestPermissionsAsync({
+    ios: {
+      allowAlert: true,
+      allowBadge: true,
+      allowSound: true,
+      allowAnnouncements: true,
+    },
+  });
+};
 
 export default function App() {
+  async function scheduleNotificationHandler() {
+    const hasPushNotificationPermissionGranted =
+      await allowsNotificationsAsync();
+
+    if (!hasPushNotificationPermissionGranted) {
+      await requestPermissionsAsync();
+    }
+
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: "My first local notification",
+        body: "This is the first local notification we are sending!",
+        data: { userName: "Bruh" },
+      },
+      trigger: {
+        seconds: 1,
+      },
+    });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button
+        title="Schedule Notifications"
+        onPress={scheduleNotificationHandler}
+      />
     </View>
   );
 }
