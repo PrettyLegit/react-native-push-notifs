9bb8d61365ab5eeee192c28f0fa1abcaa43293f8
Jimmy Nguyen <55465528+PrettyLegit0101@users.noreply.github.com>
2023-01-09 19:28:03 -0500

    init

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,44 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          alias: {
+            "@src": "./src",
+            "@assets": "./assets",
+            modules: "./src/modules",
+            lib: "./src/lib",
+            types: "./src/types",
+            constants: "./src/constants",
+          },
+        },
+      ],
+      "react-native-reanimated/plugin",
+    ],
+    env: {
+      production: {
+        plugins: [
+          [
+            "inline-dotenv",
+            {
+              path: ".env.production",
+            },
+          ],
+        ],
+      },
+      development: {
+        plugins: [
+          [
+            "inline-dotenv",
+            {
+              path: ".env.development",
+            },
+          ],
+        ],
+      },
+    },
   };
 };
--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { registerRootComponent } from "expo";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
@@ -13,8 +14,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+registerRootComponent(App);
